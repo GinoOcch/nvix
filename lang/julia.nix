13b07597c2ef1d2136742250d841a17c3447c09a
Gino Occhialini <gino.occhialini@gmail.com>
2024-10-28 20:46:27 -0400

    add julia

---
+++ lang/julia.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  extraPlugins = [pkgs.vimPlugins.julia-vim];
+  globals.latex_to_unicode_keymap = true;
+
+  plugins = {
+    lsp.servers.julials.enable = true;
+    julia-cell = {
+      enable = true;
+      settings.delimit_cells_by = "tags";
+
+      keymaps = {
+        silent = true;
+        clear = "<Leader>jl";
+        executeCell = "<Leader>jc";
+        executeCellJump = "<Leader>jC";
+        nextCell = "<Leader>jn";
+        prevCell = "<Leader>jp";
+        run = "<Leader>jr";
+      };
+    };
+  };
+}
