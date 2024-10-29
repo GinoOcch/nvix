4d40b3686730e25e7f7f6818426fc37b8c533fb3
Gino Occhialini <gino.occhialini@gmail.com>
2024-10-28 21:40:47 -0400

    julia formatting

--- lang/julia.nix
+++ lang/julia.nix
@@ -2,13 +2,30 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  jl_ls = with pkgs; (
+    julia.withPackages
+    [
+      "LanguageServer"
+      "JuliaFormatter"
+    ]
+  );
+in {
   extraPlugins = [pkgs.vimPlugins.julia-vim];
   globals.latex_to_unicode_keymap = true;
 
   plugins = {
     lsp.servers.julials.enable = true;
-    lsp.servers.julials.package = pkgs.julia;
+    lsp.servers.julials.package = jl_ls;
+
+    conform-nvim.settings = {
+      formatters_by_ft.julia = ["julia"];
+      formatters.julia = {
+        command = "${lib.getExe jl_ls}";
+        args = ["--startup-file=no" "--color=no" "-e" "using JuliaFormatter;  print(format_text(String(read(stdin))))"];
+      };
+    };
+
     julia-cell = {
       enable = true;
       settings.delimit_cells_by = "tags";
