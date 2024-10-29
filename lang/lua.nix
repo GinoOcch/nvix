43e77eca6eb01367ce0417d0d8dc229a0ba0d07b
Gino Occhialini <gino.occhialini@gmail.com>
2024-10-28 20:10:20 -0400

    update

--- lang/lua.nix
+++ lang/lua.nix
@@ -2,10 +2,10 @@
   plugins = {
     lsp.servers.lua_ls.enable = true;
     conform-nvim.settings = {
-      formatters_by_ft.lua = [ "stylua" ];
+      formatters_by_ft.lua = ["stylua"];
       formatters.stylua = {
         # If a command is string then it will execute the system's stylua binary
-        command = "stylua";
+        command = "${lib.getExe pkgs.stylua}";
       };
     };
   };
--- lang/python.nix
+++ lang/python.nix
@@ -5,8 +5,15 @@
       enable = true;
       settings = {
         pyright.disableOrganizeImports = true;
-        python.analysis.ignore = [ "*" ];
+        python.analysis.ignore = ["*"];
       };
+    };
+  };
+
+  plugins.conform-nvim.settings = {
+    formatters_by_ft.python = ["black"];
+    formatters.black = {
+      command = "${lib.getExe pkgs.black}";
     };
   };
 }
